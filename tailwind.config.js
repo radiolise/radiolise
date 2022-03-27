fa9e918edb37170a5e38a9aa625688ab4c1212e2
Marco Bauer <celsiuswr@gmail.com>
2022-03-28 01:19:54 +0200

    Set up Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.{js,jsx,vue,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+    container: false,
+  },
+  plugins: [],
+};
